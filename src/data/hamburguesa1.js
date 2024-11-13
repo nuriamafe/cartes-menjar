00521fda7fe7ee9c75b5a87413d6392f6ca47cc6
nuriamafe <nuriamafe256@gmail.com>
2024-11-13 20:15:41 +0100

    Change url

---
+++ src/data/hamburguesa1.js
@@ -0,0 +1,26 @@
+export const hamburguesa1 = [
+  {
+    name: "Hamburgueseria",
+    description: "Las mejores hamburguesas, al mejor precio",
+    phone: "668 51 85 23",
+    address: "Carrer Mercat 9 Bajo - Sueca",
+    sections: [
+      {
+        id: 0,
+        name: "Entrantes",
+        items: [
+          {
+            id: 0,
+            name: "Chinken wings",
+            description:
+              "Alitas de pollo bañadas con nuestra salsa gochu ligeramente picante con semillas de sésamo",
+            image: "",
+            price: "8€",
+            extras: [],
+            allergens: [],
+          },
+        ],
+      },
+    ],
+  },
+];
